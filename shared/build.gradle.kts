b0b5be7d6ea015c853a0928eb964f42777dae0e6
adnanawan12 <adnankhurshid.awan@gmail.com>
2023-09-21 17:13:16 +0500

    Kamel and Ktor integration

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -28,6 +28,8 @@ kotlin {
                 implementation(compose.material)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
+                implementation("media.kamel:kamel-image:0.7.3")
+                implementation("io.ktor:ktor-client-core:2.3.4")
             }
         }
         val androidMain by getting {
@@ -35,6 +37,7 @@ kotlin {
                 api("androidx.activity:activity-compose:1.7.2")
                 api("androidx.appcompat:appcompat:1.6.1")
                 api("androidx.core:core-ktx:1.10.1")
+                implementation("io.ktor:ktor-client-android:2.3.4")
             }
         }
         val iosX64Main by getting
@@ -45,6 +48,10 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:2.3.4")
+            }
+
         }
         val desktopMain by getting {
             dependencies {
