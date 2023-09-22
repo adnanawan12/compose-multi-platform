f0cb7713aa33b74b1e2f247e987c7fbf7aaebb22
adnanawan12 <adnankhurshid.awan@gmail.com>
2023-09-22 11:44:43 +0500

    Moko MVVM and LazyVerticalGrid

---
+++ shared/src/commonMain/kotlin/viewmodel/BirdsViewModel.kt
@@ -0,0 +1,61 @@
+package viewmodel
+
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import model.BirdImage
+
+
+data class BirdsUiState(
+    val images: List<BirdImage> = emptyList()
+)
+
+class BirdsViewModel:ViewModel() {
+
+    private val _uiState = MutableStateFlow<BirdsUiState>(BirdsUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        updateImages()
+    }
+
+
+   private val httpClient = HttpClient {
+        install(ContentNegotiation){
+            json()
+        }
+    }
+
+    private suspend fun getImages():List<BirdImage>{
+
+        val images = httpClient
+            .get("https://sebi.io/demo-image-api/pictures.json")
+            .body<List<BirdImage>>()
+
+        return images
+
+    }
+
+    fun updateImages(){
+        viewModelScope.launch {
+           val images = getImages()
+
+            _uiState.update {
+                it.copy(images = images)
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        httpClient.close()
+    }
+
+}
